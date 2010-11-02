3a75d2747888ec0cdf48e4b2c5f3ccc5a8ddecbc
Peter J.B. Lewis <mygit@pjblewis.com>
2010-11-02 11:28:34 +0000

    IE detection

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 include('mobile-detect.php');
 
-if ( !detect_mobile_device() && !isset($_GET['lite']) )
+if ( !detect_mobile_device() && !isset($_GET['lite']) && strstr($_SERVER['HTTP_USER_AGENT'], "MSIE") === FALSE )
 {
 	include('raycast.html');
 }
