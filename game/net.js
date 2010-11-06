75234a6ccb89834a2bf7b54ca530cb033e79428d
Peter J.B. Lewis <mygit@pjblewis.com>
2010-11-06 12:25:14 +0000

    Eclipse + SVN = BOOM

--- game/net.js
+++ game/net.js
@@ -5,7 +5,12 @@ var _net_client = null;
 
 var _net_port = 44444;
 
-function net_disconnect()
+function net_init(applet)
+{
+	_net_api = applet;
+}
+
+function net_closeServer()
 {	
 	if ( _net_api == null )
 	{
@@ -14,55 +19,60 @@ function net_disconnect()
 	
 	if (_net_server != null)
 	{
-		try { _net_api.close(_net_server); } catch(e) {}
+		try { _net_server.close(); } catch(e) {}
 		_net_server = null;
+	}
+}
+
+function net_disconnectClient()
+{	
+	if ( _net_api == null )
+	{
+		return;
 	}
 	
 	if (_net_client != null)
 	{
-		try { _net_api.close(_net_client); } catch(e) {}
+		try { _net_client.close(); } catch(e) {}
 		_net_client = null;
 	}
-	
-	_net_api = null;
 }
 
-function net_connect(applet, host)
+function net_connect(host)
 {
-	net_disconnect();
-	_net_api = applet;
-	
-	if ( applet == null )
+	if ( _net_api == null )
 	{
 		return false;
 	}
 	
+	net_disconnectClient();
+	
 	try
 	{
-		_net_client = applet.connect(host, _net_port);
-		log("Connected to " + _net_api.getRemoteIP(_net_client) + ":" + _net_api.getRemotePort(_net_client));
+		_net_client = _net_api.connect(host, _net_port);
+		log("Connected to " + _net_client.getRemoteIP(_net_client) + ":" + _net_client.getRemotePort(_net_client));
 	}
 	catch (e)
 	{
-		log("Failed to connect to " + host + ":" + _net_port + ".");
+		log("Failed to connect to " + host + ":" + _net_port + ". (" + e.message + ")");
 	}
 }
 
-function net_startServer(applet)
+function net_startServer()
 {
-	net_disconnect();
-	_net_api = applet;
-	
-	if ( applet == null )
+	if ( _net_api == null )
 	{
 		return false;
 	}
 	
-	log("This computer's address is: " + applet.getLocalIP() );
+	net_disconnectClient();
+	net_closeServer();
+	
+	log("This computer's address is: " + _net_api.getLocalIP() );
 	
 	try
 	{
-		_net_server = applet.listen(_net_port);
+		_net_server = _net_api.listen(_net_port);
 	}
 	catch (e)
 	{
@@ -77,7 +87,7 @@ function net_startServer(applet)
 	
 	log("Server started on port " + _net_port);
 	
-	return net_connect(applet, "localhost");
+	return net_connect("localhost");
 }
 
 function net_update()
@@ -88,11 +98,11 @@ function net_update()
 	}
 	
 	// Check for disconnection
-	if (!_net_api.isConnected(_net_client))
+	if (!_net_client.isConnected())
 	{
 		// connection dropped
 		log("Connection dropped.");
-		net_disconnect();
+		net_disconnectClient();
 		return;
 	}
 	
