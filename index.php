fc79efbeeb6096f6cbe43b93c113a0dd2871e72d
Peter J.B. Lewis <mygit@pjblewis.com>
2011-03-24 18:08:02 +0000

    Warn on bad user agent.

--- index.php
+++ index.php
@@ -5,8 +5,10 @@ include('mobile-detect.php');
 function is_ok_browser()
 {
 	$agent = $_SERVER['HTTP_USER_AGENT'];
-	return	strstr($agent, "MSIE") === FALSE &&
-			strstr($agent, "Opera") === FALSE;
+	return strstr($agent, "Chrome") !== FALSE;
+//	return	strstr($agent, "MSIE") === FALSE &&
+//			strstr($agent, "Opera") === FALSE &&
+//			strstr($agent, "FireFox") === FALSE;
 }
 
 if ( isset($_GET['idontcare']) || (!detect_mobile_device() && !isset($_GET['lite']) && is_ok_browser()))
