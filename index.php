c9d9b88ed46b850dd6100a514f0b81979353361f
Peter J.B. Lewis <mygit@pjblewis.com>
2011-03-25 15:07:44 +0000

    JSDC is OK in FF4

--- index.php
+++ index.php
@@ -5,7 +5,8 @@ include('mobile-detect.php');
 function is_ok_browser()
 {
 	$agent = $_SERVER['HTTP_USER_AGENT'];
-	return strstr($agent, "Chrome") !== FALSE;
+	return	strstr($agent, "Chrome") !== FALSE ||
+			strstr($agent, "Firefox/4.0") !== FALSE;
 //	return	strstr($agent, "MSIE") === FALSE &&
 //			strstr($agent, "Opera") === FALSE &&
 //			strstr($agent, "FireFox") === FALSE;
