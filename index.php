8fedcffdf4c242737042e3e60fbe01018506823b
Peter <me@pjblewis.com>
2013-01-04 11:57:45 -0800

    Adding support for IE10 as it 'works'.

--- index.php
+++ index.php
@@ -6,7 +6,8 @@ function is_ok_browser()
 {
 	$agent = $_SERVER['HTTP_USER_AGENT'];
 	return	strstr($agent, "Chrome") !== FALSE ||
-			strstr($agent, "Firefox/4.0") !== FALSE;
+			strstr($agent, "Firefox/4.0") !== FALSE ||
+			strstr($agent, "MSIE 10.0") !== FALSE;
 //	return	strstr($agent, "MSIE") === FALSE &&
 //			strstr($agent, "Opera") === FALSE &&
 //			strstr($agent, "FireFox") === FALSE;
